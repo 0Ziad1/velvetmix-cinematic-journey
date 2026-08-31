c6ff772c61f530a6b1aa8ae7b0ccf042ff0748ad
ziad <ziadmogadam@gmail.com>
2026-08-31 20:48:17 +0300

    z

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -124,7 +124,7 @@ function RootShell({ children }: { children: ReactNode }) {
 
 function RootComponent() {
   const { queryClient } = Route.useRouteContext();
-
+//
   return (
     <QueryClientProvider client={queryClient}>
       {/* Required: nested routes render here. Removing <Outlet /> breaks all child routes. */}
